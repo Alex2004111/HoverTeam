44b6c955779daea7b2b858cfb2494c9aafd8b52f
Moad Hatim <Moad4hatim@gmail.com>
2025-04-21 22:13:29 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,12 +1,13 @@
-<?php
-$dsn = 'mysql:host=localhost;dbname=student;port=3306;charset=utf8';
-$user = 'root';
-$pass = '';
-
-try {
-    $db = new PDO($dsn, $user, $pass);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Database Connection Failed: " . $e->getMessage());
-}
+<?php   
+    $check = $db->prepare('SELECT COUNT(*) FROM info WHERE id = :id');
+    $check->execute(['id' => $_SESSION['idd']]);
+    $exists = $check->fetchColumn();
+    if ($exists === 0) {
+        $deleteError = '<p class="verification">Cet ID n\'existe pas !</p>';
+    }else {
+        $delete = $db->prepare('DELETE FROM info WHERE id = :id');
+        $delete->execute(['id' => $_SESSION['idd']]);
+    }
+    
 ?>
+
